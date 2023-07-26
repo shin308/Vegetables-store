c0712c35be7310f16ace7c74abeaad848a1be8af
mahoa1103 <82137657+mahoa1103@users.noreply.github.com>
2023-07-26 09:17:45 +0700

    delete

--- OrganicApplication/src/main/java/group4/organicapplication/exception/CategoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package group4.organicapplication.exception;
-
-public class CategoryNotFoundException extends Throwable{
-    public CategoryNotFoundException(String message){
-        super(message);
-    }
-}
--- OrganicApplication/src/main/java/group4/organicapplication/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package group4.organicapplication.repository;
-
-import group4.organicapplication.model.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Integer> {
-//    public long countByID(Integer categoryID);
-}

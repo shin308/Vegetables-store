4551a4b1b5fd58718a46260449c8c890feb91e4b
Nhung04102002 <doanthaonhung@gmail.com>
2023-08-02 10:57:22 +0700

    Update Home Client, nvarchar type(name, address)

--- src/main/java/group4/organicapplication/model/Product.java
+++ src/main/java/group4/organicapplication/model/Product.java
@@ -19,7 +19,7 @@ public class Product {
     @JoinColumn(name = "categoryID")
     private Category category;
 
-    @Column(name = "productName", nullable = false)
+    @Column(name = "productName", columnDefinition = "nvarchar(100) not null")
     private String productName;
 
     @Column(name = "img")
@@ -34,7 +34,7 @@ public class Product {
     @Column(name = "quantity", nullable = false)
     private Integer quantity;
 
-    @Column(name = "description", length = 300)
+    @Column(name = "description", columnDefinition = "nvarchar(300)")
     private String description;
 
 }
